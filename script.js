2931f27e534d91ad44bdc5e6a24874fce8e48f36
Sergio Ugarte <sergiouga85@gmail.com>
2023-06-11 22:31:15 -0300

    tercera pre Entrega

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-/*CALCULADORA DE VALOR OHMICO PARA RESISTENCIAS DE CUATRO, CINCO Y SEIS BANDAS DE COLORES*/
 
-/* ARREGLO DE OBJETOS*/
+let btn_Cuatro_Bandas= document.getElementById("btnCuatroBandas");
+
+let btn_Cinco_Bandas= document.getElementById("btnCincoBandas");
+
+let btn_Seis_Bandas= document.getElementById("btnCincoBandas");
+
+let cuatroBandas;
+
+
+btn_Cuatro_Bandas.addEventListener("click",function(e){
+
+    console.log("Seleccionaste la pestaña para calcular Resistencia de 4 bandas");  
+    
+});
+
+let selectorPrimerColor = document.getElementById("primeraBanda");
+let selectorSegundoColor = document.getElementById("segundaBanda");
+let selectorTercerColor = document.getElementById("terceraBanda");
+let selectorCuartoColor = document.getElementById("cuartaBanda");
+    
+    
+let primerColor= selectorPrimerColor.addEventListener("change",function(e){
+        primerColor=selectorPrimerColor.value;
+        return primerColor;
+});
+
+
+let segundoColor= selectorSegundoColor.addEventListener("change",function(e){
+        segundoColor= selectorSegundoColor.value;
+        return segundoColor;
+});
+
+
+let tercerColor= selectorTercerColor.addEventListener("change",function(e){
+        tercerColor= selectorTercerColor.value;
+        return tercerColor;
+});
+
+
+let cuartoColor= selectorCuartoColor.addEventListener("change",function(e){
+        cuartoColor= selectorCuartoColor.value;
+        return cuartoColor;
+});   
+
 
 let Resistencia=[
     {color:'NEGRO' , numero:'0', multiplicador:1, unidad:'Ω'},
@@ -48,36 +90,6 @@ class resistencia {
         this.sestoColor= sestoColor;
     }
 
-    getDatosCuatroBandas(){
-        console.log('-----------------------------------------------------------------------------------------');
-        console.log('               Ingreso el color', this.primerColor , ' en la primera banda               ');
-        console.log('               Ingreso el color', this.segundoColor , ' en la segunda banda              ');
-        console.log('               Ingreso el color', this.tercerColor , ' en la tercera banda               ');
-        console.log('               Ingreso el color', this.cuartoColor , ' en la cuarta banda                ');
-        console.log('-----------------------------------------------------------------------------------------');
-    }
-
-    getDatosCincoBandas(){
-        console.log('-----------------------------------------------------------------------------------------');
-        console.log('               Ingreso el color', this.primerColor , ' en la primera banda               ');
-        console.log('               Ingreso el color', this.segundoColor , ' en la segunda banda              ');
-        console.log('               Ingreso el color', this.tercerColor , ' en la tercera banda               ');
-        console.log('               Ingreso el color', this.cuartoColor , ' en la cuarta banda                ');
-        console.log('               Ingreso el color', this.quintoColor , ' en la quinta banda                ');
-        console.log('-----------------------------------------------------------------------------------------');
-    }
-
-    getDatosSeisBandas(){
-        console.log('-----------------------------------------------------------------------------------------');
-        console.log('               Ingreso el color', this.primerColor , ' en la primera banda               ');
-        console.log('               Ingreso el color', this.segundoColor , ' en la segunda banda              ');
-        console.log('               Ingreso el color', this.tercerColor , ' en la tercera banda               ');
-        console.log('               Ingreso el color', this.cuartoColor , ' en la cuarta banda                ');
-        console.log('               Ingreso el color', this.quintoColor , ' en la quinta banda                ');
-        console.log('               Ingreso el color', this.sestoColor , ' en la quinta banda                 ');
-        console.log('-----------------------------------------------------------------------------------------');
-    }
-
     calcularCuatroBandas(){
 
         let bandaUno = Resistencia.find((obj_Resistencia)=> obj_Resistencia.color == this.primerColor).numero;
@@ -88,96 +100,68 @@ class resistencia {
         return ValorDeResistencia
     }
 
-    calcularCincoBandas(){
-
-        let bandaUno = Resistencia.find((obj_Resistencia)=> obj_Resistencia.color == this.primerColor).numero;
-        let bandaDos = Resistencia.find((obj_Resistencia)=> obj_Resistencia.color == this.segundoColor).numero; 
-        let bandaTres = Resistencia.find((obj_Resistencia)=> obj_Resistencia.color == this.tercerColor).numero;
-        let bandaCuatro = Resistencia.find((obj_Resistencia)=> obj_Resistencia.color == this.cuartoColor).multiplicador;
-        let Unidad= Resistencia.find((obj_Resistencia)=> obj_Resistencia.color == this.tercerColor).unidad;
-        let ValorDeResistencia = (bandaUno+bandaDos+bandaTres)* bandaCuatro + Unidad;
-        return ValorDeResistencia
-    }
-
     ToleranciaCuatroBandas(){
         
         let bandaCuatro = Tolerancia.find((obj_Tolerancia)=> obj_Tolerancia.color == this.cuartoColor).porcentaje;
         return bandaCuatro
     }
-
-    ToleranciaCincoBandas(){
-        
-        let bandaCinco = Tolerancia.find((obj_Tolerancia)=> obj_Tolerancia.color == this.quintoColor).porcentaje;
-        return bandaCinco
-    }
-
-    coeficienteDeTemperatura(){
-        let bandaSeis = PPM.find((obj_PPM)=> obj_PPM.color == this.sestoColor).coeficiente;
-        return bandaSeis
-    }
-
 }
-let listaResistencia =[];
-let listaValorResistencia=[];
-let listagetDatos=[];
-let verResistencia;
+
 let nuevaResistencia;
+let valor;
 
 
-console.log('BIENVENIDOS A LA CALCULADORA DE VALOR OHMICO PARA RESISTENCIAS DE CUATRO , CINCO Y SEIS BANDAS');
-let bandas='0';
 
-do{   
-    bandas= prompt('Ingrese la cantidad de bandas de colores de la resistencia a calcular o sino salir').toUpperCase();
+function calcular(){
+    nuevaResistencia = new resistencia(primerColor,segundoColor,tercerColor,cuartoColor);
+    console.log (nuevaResistencia.calcularCuatroBandas(),nuevaResistencia.ToleranciaCuatroBandas());  
+    mostrarValor=document.getElementById("valorResistivo");
+    mostrarValor.value= (nuevaResistencia.calcularCuatroBandas() +" "+ nuevaResistencia.ToleranciaCuatroBandas());
+}
 
-    if(bandas==4){
 
-        primerColor= prompt('Ingresa el color de la primera banda').toUpperCase();
-        segundoColor= prompt('Ingresa el color de la segunda banda').toUpperCase();
-        tercerColor= prompt('Ingresa el color de la tercera banda').toUpperCase();
-        cuartoColor= prompt('Ingresa el color de la cuarta banda').toUpperCase();
-        /*creo una nueva instancia de objetos*/
-        nuevaResistencia = new resistencia(primerColor,segundoColor,tercerColor,cuartoColor);
-        listagetDatos.push(nuevaResistencia.getDatosCuatroBandas());
-        listaResistencia.push('Resistencia de ',nuevaResistencia.calcularCuatroBandas(),'',nuevaResistencia.ToleranciaCuatroBandas(),'de tolerancia  ');
-        verResistencia=listaResistencia.join(' ');  
-        console.log('Resistencia de ',nuevaResistencia.calcularCuatroBandas(),'',nuevaResistencia.ToleranciaCuatroBandas(),'de tolerancia  ');
-         
-    }
+let btnCalcular= document.getElementById("btnCalcular");
+
+btnCalcular.addEventListener("click", calcular); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    else if(bandas==5){
-
-        primerColor= prompt('Ingresa el color de la primera banda').toUpperCase();
-        segundoColor= prompt('Ingresa el color de la segunda banda').toUpperCase();
-        tercerColor= prompt('Ingresa el color de la tercera banda').toUpperCase();
-        cuartoColor= prompt('Ingresa el color de la cuarta banda').toUpperCase();
-        quintoColor= prompt('Ingresa el color de la quinta banda').toUpperCase();
-        /*creo una nueva instancia de objetos*/
-        nuevaResistencia = new resistencia(primerColor,segundoColor,tercerColor,cuartoColor,quintoColor);
-        listagetDatos.push(nuevaResistencia.getDatosCincoBandas());
-        listaResistencia.push('Resistencia de ',nuevaResistencia.calcularCincoBandas(),'',nuevaResistencia.ToleranciaCincoBandas(),'de tolerancia');
-        verResistencia=listaResistencia.join(' ');  
-        console.log('Resistencia de ',nuevaResistencia.calcularCincoBandas(),'',nuevaResistencia.ToleranciaCincoBandas(),'de tolerancia');
-         
-    }
-    else if(bandas==6){
-
-        primerColor= prompt('Ingresa el color de la primera banda').toUpperCase();
-        segundoColor= prompt('Ingresa el color de la segunda banda').toUpperCase();
-        tercerColor= prompt('Ingresa el color de la tercera banda').toUpperCase();
-        cuartoColor= prompt('Ingresa el color de la cuarta banda').toUpperCase();
-        quintoColor= prompt('Ingresa el color de la quinta banda').toUpperCase();
-        sestoColor= prompt('Ingresa el color de la sesta banda').toUpperCase();
-        /*creo una nueva instancia de objetos*/
-        nuevaResistencia = new resistencia(primerColor,segundoColor,tercerColor,cuartoColor,quintoColor,sestoColor);
-        listagetDatos.push(nuevaResistencia.getDatosSeisBandas());
-        listaResistencia.push('Resistencia de ',nuevaResistencia.calcularCincoBandas(),'',nuevaResistencia.ToleranciaCincoBandas(),'de tolerancia con un coeficiente de temperatura de', nuevaResistencia.coeficienteDeTemperatura());
-        verResistencia=listaResistencia.join(' ');  
-        console.log('Resistencia de ',nuevaResistencia.calcularCincoBandas(),'',nuevaResistencia.ToleranciaCincoBandas(),'de tolerancia con un coeficiente de temperatura de', nuevaResistencia.coeficienteDeTemperatura());
-         
-    }
-} 
-while (bandas !='SALIR');
 
 
 
@@ -186,4 +170,5 @@ while (bandas !='SALIR');
 
 
 
- 
+
+
